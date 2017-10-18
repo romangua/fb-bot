b5cc7fcad152dfdefbbdb34ba07717faddf99618
romangua <dev3@dev2s-MacBook-6.local>
2017-10-18 13:41:46 -0300

    commit inicial

---
+++ functions/index.js
@@ -0,0 +1,244 @@
+'use strict';
+
+const express = require('express');
+const firebase = require('firebase');
+const functions = require('firebase-functions');
+const bodyParser = require('body-parser');
+const request = require('request');
+const fetch = require('node-fetch');
+const crypto = require('crypto');
+const {Wit, log} = require('node-wit');
+const process = require('./process');
+
+// Wit.ai parameters
+const WIT_TOKEN = "MFZICCEHCMS2DXKNQMLZFN4XDMAIP4GF";
+
+//Messenger API parameters
+const FB_PAGE_TOKEN = "EAACkkvctpEwBABa80xLN7MSdnU7JzuoBAAg5chUiIcEIE8Gq1v4OboHKCRcZBQ4q2MOAGofrKP53N0PnOC3zduyzI5nP9mJ1nf33usfA9ELtuYwfvGVqIoUrbKpptxsZCkkdLvS5adF58G0FrWrZATLBEDUe3IwFc8z5ZAfoDgZDZD";
+const FB_APP_SECRET = "002a1bc533d92ffa4ff370b16ce00b8e";
+let FB_VERIFY_TOKEN = null;
+crypto.randomBytes(8, (err, buff) => {
+  if (err) throw err;
+  FB_VERIFY_TOKEN = buff.toString('hex');
+});
+
+//Firebase parameters
+var config = {
+    apiKey: "AIzaSyCAgdg4LNIIhe_9o8XnGagKSsCkIW2yI_w",
+    authDomain: "facebook-bot-a6f58.firebaseapp.com",
+    databaseURL: "https://facebook-bot-a6f58.firebaseio.com",
+    projectId: "facebook-bot-a6f58",
+    storageBucket: "facebook-bot-a6f58.appspot.com",
+    messagingSenderId: "579671298783"
+};
+firebase.initializeApp(config);
+
+// Setting up our bot
+const clientWit = new Wit({accessToken: WIT_TOKEN});
+
+// Starting our webserver
+const app = express();
+app.use(({method, url}, rsp, next) => {
+  rsp.on('finish', () => {
+    console.log(`${rsp.statusCode} ${method} ${url}`);
+  });
+  next();
+});
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+
+// ----------------------------------------------------------------------------
+// Messenger API specific code
+// ----------------------------------------------------------------------------
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      entry.messaging.forEach(function(event) {
+        if (event.postback) {
+          receivedPostback(event);
+        } else if (event.message && event.message.quick_reply) {
+          receivedQuickReply(event);
+        }
+        // Si es un mensaje de texto y no es enviado por mi mismo lo analizo con wit
+        else if (event.message && event.message.text) {
+          receivedMessage(event);
+        }
+        else {
+          console.log("Webhook received unknown event: ", JSON.stringify(entry));
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var message = event.message;
+
+  console.log("receivedMessage: " + JSON.stringify(event));
+
+  if (message.text) {
+    clientWit.message(message.text, {})
+    .then((data) => {
+      callSendAPI(process.processText(event, data));
+    })
+    .catch(console.error);
+  }
+  else if (message.attachments) {
+    callSendAPI(sendTextMessage(senderID, "Por el momento no podemos procesar mensages con archivos adjuntos."));
+  }
+}
+
+function receivedPostback(event) {
+  console.log("receivedPostback: " + JSON.stringify(event));
+
+  var responseProcess = process.processPostBack(event);
+  callSendAPI(responseProcess);
+}
+
+function receivedQuickReply(event) {
+  console.log("receivedQuickReply: " + JSON.stringify(event));
+
+  var responseProcess = process.processQuickReply(event);
+  callSendAPI(responseProcess);
+}
+
+function callSendAPI(messageData) {
+  console.log("Mensaje a enviar: " + JSON.stringify(messageData));
+
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: FB_PAGE_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
+
+function sendTextMessage (recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+  return messageData;
+}
+
+function sendGenericMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+// Webhook setup
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * the App Dashboard, we can verify the signature that is sent with each
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', FB_APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
+
+exports.app = functions.https.onRequest(app);
---
+++ functions/process.js
@@ -0,0 +1,189 @@
+const _ = require('lodash');
+
+const MENSAJE_INICIAL = "mensaje_inicial";
+const EMPEZAR_DE_NUEVO_PAYLOAD = "EMPEZAR_DE_NUEVO_PAYLOAD";
+const EMPEZAR_PAYLOAD = "EMPEZAR";
+const EMPEZAR_ARMO_MI_VIAJE_PAYLOAD = "EMPEZAR_ARMO_MI_VIAJE_PAYLOAD";
+const EMPEZAR_SORPRENDEME_PAYLOAD = "EMPEZAR_SORPRENDEME_PAYLOAD";
+const ARMO_MI_VIAJE_PAYLOAD = "ARMO_MI_VIAJE_PAYLOAD";
+const ARMO_MI_VIAJE_VUELOS_PAYLOAD = "ARMO_MI_VIAJE_VUELOS_PAYLOAD";
+const ARMO_MI_VIAJE_HOTELES_PAYLOAD = "ARMO_MI_VIAJE_HOTELES_PAYLOAD";
+
+exports.processText = function(event, data) {
+  var messageText = "No se reconoce el texto ingresado. Reintente por favor";
+
+  if(!_.isEmpty(data.entities)) {
+
+    console.log("Respuesta wit: " + JSON.stringify(data));
+    // Tipo fecha
+    if(data.entities.datetime) {
+      data.entities.datetime.forEach(function(datetime) {
+        var confidence = datetime.confidence;
+        var value = datetime.value;
+
+        messageText = confidence + " " + value;
+      });
+    }
+    // Tipo location
+    else if(data.entities.location) {
+        var confidence = data.entities.location.confidence;
+        var value = data.entities.location.value;
+        return ciudadIndicadaMessage(event.sender.id);
+    }
+    // Texto sin intent especial
+    else if(data.entities.intent) {
+      data.entities.intent.forEach(function(intent) {
+        var confidence = intent.confidence;
+        var value = intent.value;
+
+        if(confidence > 0.7) {
+          if(value == MENSAJE_INICIAL)
+            return initMessage(event.sender.id);
+        }
+      });
+    }
+  }
+  return sendTextMessage(event.sender.id, messageText);
+}
+
+exports.processPostBack = function(event) {
+  let message = "No se reconoce processPostBack";
+
+  switch (event.postback.payload) {
+    case EMPEZAR_DE_NUEVO_PAYLOAD:
+    case EMPEZAR_PAYLOAD:
+      return initMessage(event.sender.id);
+
+    case ARMO_MI_VIAJE_PAYLOAD:
+      return armoMiViajeMessage(event.sender.id);
+  }
+  return sendTextMessage(event.sender.id, message);
+}
+
+exports.processQuickReply = function(event) {
+  let message = "No se reconoce processQuickReply";
+  switch (event.message.quick_reply.payload) {
+    case EMPEZAR_SORPRENDEME_PAYLOAD:
+    case ARMO_MI_VIAJE_VUELOS_PAYLOAD:
+      message = "¿Desde qué ciudad salis?";
+      break;
+
+    case ARMO_MI_VIAJE_HOTELES_PAYLOAD:
+      message = "¿En qué ciudad te vas a hospedar?";
+      break;
+
+    case EMPEZAR_ARMO_MI_VIAJE_PAYLOAD:
+      return armoMiViajeMessage(event.sender.id);
+  }
+  return sendTextMessage(event.sender.id, message);
+}
+
+function initMessage(recipientId) {
+  let mensajeInicial = {
+  "recipient":{
+      "id":recipientId
+    },
+    "message":{
+      "text": "Hola 🙂 Soy Kevin Bot, el asistente virtual de viajes de Almundo. ¿Querés que te sorprenda o armás tu viaje?",
+      "quick_replies":
+      [
+        {
+          "content_type":"text",
+          "title":"Armo mi viaje",
+          "payload":"EMPEZAR_ARMO_MI_VIAJE_PAYLOAD"
+        },
+        {
+          "content_type":"text",
+          "title":"Sorprendeme",
+          "payload":"EMPEZAR_SORPRENDEME_PAYLOAD"
+        }
+      ]
+    }
+  };
+  return mensajeInicial;
+}
+
+function armoMiViajeMessage(recipientId) {
+  let mensaje = {
+  "recipient":{
+      "id":recipientId
+    },
+    "message":{
+      "text": "¿Qué deséas buscar?",
+      "quick_replies":
+      [
+        {
+          "content_type":"text",
+          "title":"Vuelos",
+          "payload":"ARMO_MI_VIAJE_VUELOS_PAYLOAD"
+        },
+        {
+          "content_type":"location"
+        },
+        {
+          "content_type":"text",
+          "title":"Hoteles",
+          "payload":"ARMO_MI_VIAJE_HOTELES_PAYLOAD"
+        }
+      ]
+    }
+  };
+  return mensaje;
+}
+
+function ciudadIndicadaMessage(recipientId) {
+  let mensaje = {
+  "recipient":{
+      "id": recipientId
+  },
+  "message":{
+      "text": "¿A cuál de estos lugares te referís? Si no es ninguno de estos, verificá el nombre de la ciudad y volvé a enviarlo.",
+      "quick_replies":
+      [
+        {
+          "content_type":"text",
+          "title":"Cordoba, Cordoba",
+          "payload":"PAYLOAD"
+        },
+        {
+          "content_type":"text",
+          "title":"Marcos Juares, Cordoba",
+          "payload":"PAYLOAD"
+        },
+        {
+          "content_type":"text",
+          "title":"Leones, Cordoba",
+          "payload":"PAYLOAD"
+        },
+        {
+          "content_type":"text",
+          "title":"Villa Maria, Cordoba",
+          "payload":"PAYLOAD"
+        },
+        {
+          "content_type":"text",
+          "title":"BellVille, Cordoba",
+          "payload":"PAYLOAD"
+        },
+        {
+          "content_type":"text",
+          "title":"Carlos Paz, Cordoba",
+          "payload":"PAYLOAD"
+        }
+      ]
+    }
+  };
+  return mensaje;
+}
+
+function sendTextMessage (recipientId, messageText) {
+  let messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+  return messageData;
+}
